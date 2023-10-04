c46c808fd81859f2b090bd902eeb2d97203dd31c
AleCarb <alecarb36@gmail.com>
2023-10-03 21:22:31 -0300

    agrego Navbar y Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Navbar from './components/Navbar'
 import Product from './components/Product';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Product></Product>
+      <Footer></Footer>
     </div>
   );
 }
