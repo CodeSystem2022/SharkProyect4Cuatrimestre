156edc5064d0d135c8c65fd951e42192379b0c8b
Magali Cardozo <magascard1@gmail.com>
2023-10-18 14:26:58 -0300

    cambios en el App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-
 import './App.css';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer';
 import Products from './components/Products';
+import Product from './components/Product';
 import CheckoutPage from './components/CheckoutPage';
-
 import { ThemeProvider } from '@mui/material/styles';
 import { createMyTheme } from './theme';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 // Define tu tema aquí
 const theme = createMyTheme();
 
 function App() {
   return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <Navbar></Navbar>
-        {/*<CheckoutPage></CheckoutPage>*/}
-        <Products></Products>
-        <Product></Product>
-        <Footer></Footer>
-      </ThemeProvider>
-    </div>
+    <Router>
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <Navbar/>
+          <Routes>
+            <Route path="/checkout-page" element={<CheckoutPage/>} />
+            <Route path="/" element={<Products/>} />
+          </Routes>
+          <Footer></Footer>
+        </ThemeProvider>
+      </div>
+    </Router>
   );
 }
 
