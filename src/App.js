4457e24a70e4b9eb073d7914a4dfefbb8f63776b
Magali Cardozo <magascard1@gmail.com>
2023-10-19 00:38:43 -0300

    Arreglo unas cositas

--- src/App.js
+++ src/App.js
@@ -33,21 +33,21 @@ export default App;
 import Navbar from './components/Navbar';
 import Products from './components/Products';
 import CheckoutPage from './components/CheckoutPage';
-import {Switch, BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Navbar/>
       <Router>
-        <Switch>
+       <Navbar/>
+        <Routes>
           <Route path='/checkout-page'>
            <CheckoutPage/>
           </Route>
           <Route path='/'>
             <Products/>
           </Route>
-        </Switch>
+        </Routes>
       </Router>
      
     </div>
--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,14 +1,13 @@
+//estamos creando el context API para poder pasar las variables de un componente a otro
 import { createContext, useContext, useReducer } from "react";
 
-export const StateContext = createContext(); //Permite crear el contexto
-
-//Provee la herramienta para pasar los datos de un componente a otro
-export const StateProvider =({reducer, initialState, children}) => (
+export const StateContext = createContext();
+//state provider nos ds la herramienta para pasar los datos de un comp a otro
+export const StateProvider = ({ reducer, initialState, children }) => (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
 );
-
-export const useStateValue = ()=> useContext(StateContext); // useStateValue permite consumir los cambios de estado de InitialSate desde cualquier componente
-
-//aqui creamos el CONTEXT API
+//useState va a peermitirnos usar los cambios desde cualquier componente
+export const useStateValue = () => useContext(StateContext);
+export default StateProvider;
