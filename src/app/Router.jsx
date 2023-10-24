eef1aa20e6435a9d546ec8c6d768c68a7d2be8a6
Magali Cardozo <magascard1@gmail.com>
2023-10-24 00:36:05 -0300

    SUBO LAS IMAGENES

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "../pages/Home"
 import CheckoutPage from '../components/CheckoutPage';
+import Navbar from '../components/Navbar'
 const Router = () => {
     return (
     <BrowserRouter>
