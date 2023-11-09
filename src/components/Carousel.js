79c23f88fac9651179f5e76c72c0a9a793fa17c4
Magali Cardozo <magascard1@gmail.com>
2023-11-09 12:11:13 -0300

    intento dde carrousel en un solo componente

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,80 +1,53 @@
-import React, { useEffect, useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import {data} from '../assets/data'
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-
+import { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
 import './Carousel.css';
-// import SwiperSlide from './Swiper';
-// import required modules
-import { Pagination } from 'swiper/modules';
 
-export default function Carrousel() {
- const listRef = useRef();
- const [currentIndex, setCurrentIndex] = useState(0);
+export const Carousel = ({
+  children,
+  className = "",
+  buttons = false,
+  ...props
+}) => {
+  const swiperRef = useRef();
 
- useEffect(() => {
-  const listNode = listRef.current;
-  const imgNode = listNode.querySelectorAll("li > img")[currentIndex];
+  const handlePreviousItem = () => {
+    swiperRef.current.swiper.slidePrev();
+  };
 
-  if (imgNode) {
-    imgNode.scrollIntoView({
-      behavior: "smooth"
-    });
-  }
+  const handleNextItem = () => {
+    swiperRef.current.swiper.slideNext();
+  };
 
-}, [currentIndex]);
-
-const scrollToImage = (direction) => {
-  if (direction === 'prev') {
-    setCurrentIndex(curr => {
-      const isFirstSlide = currentIndex === 0;
-      return isFirstSlide ? 0 : curr - 1;
-    })
-  } else {
-    const isLastSlide = currentIndex === data.length - 1;
-    if (!isLastSlide) {
-      setCurrentIndex(curr => curr + 1);
-    }
-  }
-}
-
-const goToSlide = (slideIndex) => {
-  setCurrentIndex(slideIndex);
-}
-return (
-  <div className="main-container">
-    <div className="slider-container">
-      <div className='leftArrow' onClick={() => scrollToImage('prev')}>&#10092;</div>
-      <div className='rightArrow' onClick={() => scrollToImage('next')}>&#10093;</div>
-      <div className="container-images">
-        <ul ref={listRef}>
-          {
-            data.map((item) => {
-              return <li key={item.id}>
-                <img src={item.imgUrl} width={300} height={300} />
-                <p>{item.text}</p>
-              </li>
-            })
-          }
-        </ul>
-      </div>
-      <div className="dots-container">
-        {
-          data.map((_, idx) => (
-            <div key={idx}
-              className={`dot-container-item ${idx === currentIndex ? "active" : ""}`}
-              onClick={() => goToSlide(idx)}>
-              &#9865;
-            </div>))
-        }
-      </div>
-    </div>
-  </div >
-)
-}
+  return (
+    <Swiper className={`relative ${className}`} ref={swiperRef} {...props}>
+      {children.map((child, i) => (
+        <SwiperSlide key={i}>{child}</SwiperSlide>
+      ))}
+      {buttons ? (
+        <>
+          <button
+            onClick={handleNextItem}
+            className="absolute right-0 top-[calc(50%-2rem)] z-50 grid h-16 w-16 place-items-center rounded-full bg-dark-gray"
+          >
+            <img src="/icons/arrow.svg" alt="Right arrow" />
+          </button>
+          <button
+            onClick={handlePreviousItem}
+            className="absolute left-0 top-[calc(50%-2rem)] z-50 grid h-16 w-16 place-items-center rounded-full bg-dark-gray"
+          >
+            <img
+              className="rotate-180"
+              src="/icons/arrow.svg"
+              alt="Left arrow"
+            />
+          </button>
+        </>
+      ) : null}
+    </Swiper>
+  );
+};
 
 
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Products from '../components/Products';
-import Carousel from '../components/Carousel';
-import Carrousel2 from '../components/Carousel2';
-import Footer from '../components/Footer';
+import React from "react";
+import { Link } from "react-router-dom";
+import Products from "../components/Products";
+import {Carousel} from "../components/Carousel";
+import Carrousel2 from "../components/Carousel2";
+import Footer from "../components/Footer";
+import products from "../product-data";
+import { data } from "../assets/data";
 
 
 // Creamos la pagina como una constante, ahorramos procesos
 const Home = () => {
-    return (
-       
-            <div>
-                {/*Seccion de productos*/}
-                <Link to="/Products">
-                    <img src="https://helix-aries-969.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F53c78f00-661f-4e94-be61-bb401036ef16%2F8636c9f5-981f-4689-86a8-156b3df65142%2Fbotontienda.jpg?table=block&id=09f05d5e-3873-4b1c-85fb-0211f86aaaa1&spaceId=53c78f00-661f-4e94-be61-bb401036ef16&width=2000&userId=&cache=v2" 
-                    height={300} //esto despues se cambia en el CSS
-                    alt="Ver tienda" />
-                </Link>
-                <Carousel/>
-                <Carrousel2/>
-                {/* ... */}
-            </div>
-       
-    );
+  return (
+    <div>
+      {/*Seccion de productos*/}
+      <Link to="/Products">
+        <img
+          src="https://helix-aries-969.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F53c78f00-661f-4e94-be61-bb401036ef16%2F8636c9f5-981f-4689-86a8-156b3df65142%2Fbotontienda.jpg?table=block&id=09f05d5e-3873-4b1c-85fb-0211f86aaaa1&spaceId=53c78f00-661f-4e94-be61-bb401036ef16&width=2000&userId=&cache=v2"
+          height={300} //esto despues se cambia en el CSS
+          alt="Ver tienda"
+        />
+      </Link>
+      <Carousel
+  className="h-[27rem]"
+  slidesPerView={1}
+  spaceBetween={30}
+//   buttons
+  navigation
+  centeredSlides
+  loop
+  autoplay={{
+    delay: 2500,
+    disableOnInteraction: true,
+  }}
+  breakpoints={{
+    880: {  
+      slidesPerView: 2,
+    },
+    1220: {
+      slidesPerView: 3,
+    },
+  }}
+>
+  {data.map((item) => {
+    return <li key={item.id}>
+      <img src={item.imgUrl} width={300} height={300} />
+      <p>{item.text}</p>
+    </li>
+  })}
+</Carousel>
+      {/* ... */}
+    </div>
+  );
 };
 
 // Exportamos página
