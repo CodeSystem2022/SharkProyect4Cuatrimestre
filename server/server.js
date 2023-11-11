6313366a22f9ae5726f36f739f6bcc4f0cdb8645
AleCarb <alecarb36@gmail.com>
2023-11-11 10:58:30 -0300

    cambio puerto para la redireccion

--- server/server.js
+++ server/server.js
@@ -25,9 +25,9 @@ app.post("/create_preference", (req, res) => {
   const preference = {
     items: items,
     back_urls: {
-      success: "http://localhost:5173/success",
-      failure: "http://localhost:5173/failure",
-      pending: "http://localhost:5173/pending",
+      success: "http://localhost:3000/success",
+      failure: "http://localhost:3000/failure",
+      pending: "http://localhost:3000/pending",
     },
     auto_return: "approved",
   };
